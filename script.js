dab7b674fc1f305b1674a6a24680b10c5569c540
kirti tiwari <kirtiwari22@gmail.com>
2025-07-16 20:56:26 +0530

    wip: adding api

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("hello");
+const apiKey = "1444e1ddb7eefe262deb2ce6d16666b4";
+
+const unitsMap = {
+  metric: {
+    temp: "°C",
+    wind: "m/s",
+  },
+  imperial: {
+    temp: "°F",
+    wind: "mph",
+  },
+};
+
+async function getWeatherData() {
+  const location = document.getElementById("userLocation").value;
+  const tempUnit = document.getElementById("tempUnit").value;
+
+  const units = tempUnit === "C" ? "metric" : "imperial";
+
+  const baseUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=${units}&q=${location}`;
+
+  const res = await (await fetch(baseUrl)).json();
+
+  console.log("response", res);
+
+  const details = {
+    name: res.name,
+    temp: res.main.temp + unitsMap[units].temp,
+    humidity: res.main.humidity,
+    wind: res.wind.speed + unitsMap[units].wind,
+  };
+
+  document.getElementsByClassName("tempValue")[0].innerHTML = details.temp;
+}
